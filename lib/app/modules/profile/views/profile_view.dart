5aa23a98bfc28fd69115c24811b285f4df42ddf7
Galuh DAP <90491250+Galuhdap@users.noreply.github.com>
2023-12-30 23:23:50 +0700

    update
    new

--- lib/app/modules/profile/views/profile_view.dart
+++ lib/app/modules/profile/views/profile_view.dart
@@ -9,39 +9,58 @@ class ProfileView extends GetView<ProfileController> {
   const ProfileView({Key? key}) : super(key: key);
   @override
   Widget build(BuildContext context) {
-   final ProfileController profileController =  Get.put(ProfileController());
+    final ProfileController profileController = Get.put(ProfileController());
     return Scaffold(
       appBar: AppBar(
         title: const Text('My Profile'),
         centerTitle: true,
       ),
-      body: 
-        Padding(
-          padding: const EdgeInsets.only(left: 25, right: 25),
-          child: Column(
-            children: [
-              FieldAddDataWidget(
-                title: "Name",
-                textController: profileController.name,
-                typeInput: TextInputType.text,
-                contentPadding: EdgeInsets.only(left: 13),
+      body: Padding(
+        padding: const EdgeInsets.only(left: 25, right: 25),
+        child: Column(
+          children: [
+            FieldAddDataWidget(
+              title: "Name",
+              textController: profileController.name,
+              typeInput: TextInputType.text,
+              contentPadding: EdgeInsets.only(left: 13),
+            ),
+            FieldAddDataWidget(
+              title: "Email",
+              textController: profileController.email,
+              typeInput: TextInputType.text,
+              contentPadding: EdgeInsets.only(left: 13),
+            ),
+            FieldAddDataWidget(
+              title: "Email Verified At",
+              textController: profileController.email,
+              typeInput: TextInputType.text,
+              contentPadding: EdgeInsets.only(left: 13),
+            ),
+            SizedBox(
+              height: 15,
+            ),
+            ElevatedButton(
+              onPressed: () {},
+              style: ElevatedButton.styleFrom(
+                  elevation: 4,
+                  backgroundColor: Colors.red,
+                  shape: RoundedRectangleBorder(
+                    borderRadius: BorderRadius.circular(20),
+                  ),
+                  minimumSize: Size(0, 50)),
+              child: Center(
+                child: Text(
+                  'LOGOUT',
+                  style: TextStyle(
+                    color: Colors.white,
+                  ),
+                ),
               ),
-              FieldAddDataWidget(
-                title: "Email",
-                textController: profileController.email,
-                typeInput: TextInputType.text,
-                contentPadding: EdgeInsets.only(left: 13),
-              ),
-              FieldAddDataWidget(
-                title: "Email Verified At",
-                textController: profileController.email,
-                typeInput: TextInputType.text,
-                contentPadding: EdgeInsets.only(left: 13),
-              ),
-            ],
-          ),
+            ),
+          ],
         ),
-     
+      ),
     );
   }
 }
